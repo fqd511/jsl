62c76bbc5a1ea9ef523959be52d573d8aeb327f2
van <fqd511@163.com>
2022-09-24 12:19:04 +0800

    add content

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -1,4 +1,13 @@
-export default {
-  title: 'Homepage',
-  description: '校长的网站'
-}
+import { defineConfig } from 'vitepress'
+
+export default defineConfig({
+  title: '三谷玩偶工作室',
+  titleTemplate:'东莞小雅文化科技有限公司',
+  description: '三谷玩偶工作室 - 东莞小雅文化科技有限公司',
+  themeConfig: {
+    footer: {
+      message: '备案号#######',
+      copyright: 'Copyright © 2019-present 三谷玩偶工作室 | 东莞市小雅文化科技有限公司'
+    }
+  }
+})
